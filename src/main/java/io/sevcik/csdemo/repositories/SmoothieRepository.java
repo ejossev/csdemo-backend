d8b0f22f6877b98c2221ffe2cb1fb47ff7bf2487
Josef Sevcik <josef.sevcik@gmail.com>
2022-12-15 21:12:03 +0100

    Native SQL queries in SmoothieRepo

--- src/main/java/io/sevcik/csdemo/repositories/SmoothieRepository.java
+++ src/main/java/io/sevcik/csdemo/repositories/SmoothieRepository.java
@@ -3,6 +3,7 @@ package io.sevcik.csdemo.repositories;
 import io.sevcik.csdemo.models.Smoothie;
 import io.sevcik.csdemo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,20 @@ import java.util.Optional;
 
 @Repository
 public interface SmoothieRepository extends JpaRepository<Smoothie, Long> {
+    @Query(
+            value = "SELECT " +
+                    "s.id as id, " +
+                    "s.name as name, " +
+                    "s.description as description, " +
+                    "n.calories as calories, " +
+                    "n.proteins as proteins, " +
+                    "n.fat as fat, " +
+                    "n.carbs as carbs " +
+                    "FROM smoothies s LEFT JOIN nutritions n " +
+                    "ON s.id=n.smoothie_id WHERE s.name= ?1",
+            nativeQuery = true)
     Optional<Smoothie> findByName(String name);
+    @Query(value = "SELECT CASE WHEN EXISTS ( SELECT 1  FROM smoothies WHERE name= ?1 ) THEN 'true' ELSE 'false' END",
+            nativeQuery = true)
     boolean existsByName(String name);
 }
